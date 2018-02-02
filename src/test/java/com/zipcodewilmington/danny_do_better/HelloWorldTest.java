5e88665bf28834f912ba4cee8e7cbf8270659352
Leon <leon@Leons-MacBook-Pro.local>
2018-02-01 23:56:11 -0500

    updated junit tests; MathUtilitiesTest

--- src/test/java/com/zipcodewilmington/danny_do_better/HelloWorldTest.java
+++ src/test/java/com/zipcodewilmington/danny_do_better/HelloWorldTest.java
@@ -1,6 +1,5 @@
 package com.zipcodewilmington.danny_do_better;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,19 +8,12 @@ import static org.junit.Assert.assertEquals;
  * Created by leon on 2/1/18.
  */
 public class HelloWorldTest {
-
-    HelloWorld helloWorld;
-
-    @Before
-    public void initialize(){
-        helloWorld = new HelloWorld();
-    }
+    public static final HelloWorld helloWorld = new HelloWorld();
 
     @Test
     public void TestHelloWorld(){
         String expected = "Hello World";
-        String actual = this.helloWorld.helloWorld();
-
+        String actual = helloWorld.helloWorld();
         assertEquals(expected,actual);
     }
 
